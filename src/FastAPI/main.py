236150f2b2e52d3f6b0f919a783e749f8f666180
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2023-12-17 02:10:37 -0300

    Es un commit normal

--- src/FastAPI/main.py
+++ src/FastAPI/main.py
@@ -13,3 +13,4 @@ async def read_root():
 async def read_root():
     return {"url_curso":"https://replit.com/"}
 
+#aca hasta ahora hiciste algunos "get" que te permiten obtener datos de un servidor y mostrarlo basicamente. 
